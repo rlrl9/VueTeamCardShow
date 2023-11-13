9acf0fbd0ca624df79202baba4647e7688b3efd4
SKY <109563617+rlrl9@users.noreply.github.com>
2023-11-13 13:40:13 +0900

    Add files via upload

---
+++ WorkView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="work">
+    <h1>송기영의 Vue 실습 페이지</h1>
+  </div>
+  <hr />
+  <TeamComponent name="송기영" food="마라탕" teamNum="1" srcUrl="../../public/images/image2.png"></TeamComponent>
+  <TeamComponent name="유승희" food="아이스크림" teamNum="1" srcUrl="../../public/images/image4.png"></TeamComponent>+
+  <TeamComponent name="이성수" food="순대국밥" teamNum="1" srcUrl="../../public/images/image3.png"></TeamComponent>
+  <TeamComponent name="전승현" food="햄버거" teamNum="1" srcUrl="../../public/images/image1.png"></TeamComponent>
+</template>
+
+<script setup>
+// @ is an alias to /src
+import TeamComponent from '@/components/TeamCard.vue'
+</script>
+
